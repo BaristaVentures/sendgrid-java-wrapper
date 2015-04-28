999006a9d6dd2a8aca578210907b2f594cb2b8c6
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2015-04-28 12:17:21 -0500

    Initial commit with the basic send email

---
+++ src/sendgrid_java_wrapper/core.clj
@@ -0,0 +1,16 @@
+(ns sendgrid-java-wrapper.core
+  (:import (com.sendgrid SendGrid
+                         SendGrid$Email
+                         SendGrid$Response)))
+
+(defn send-email
+  [{username :api_user password :api_key} {to :to from :from subject :subject html :html}]
+  (let [sendgrid (SendGrid. username password)
+        email (-> (SendGrid$Email.)
+                  (.addTo to)
+                  (.setFrom from)
+                  (.setSubject subject)
+                  (.setHtml html))
+        response (.send sendgrid email)]
+    (.getMessage response)))
+
---
+++ test/sendgrid_java_wrapper/core_test.clj
@@ -0,0 +1,8 @@
+(ns sendgrid-java-wrapper.core-test
+  (:use midje.sweet)
+  (:require [sendgrid-java-wrapper.core :refer :all]))
+
+(facts "abouts sendrid java wrapper"
+       (fact "it needs tests"
+             0 => 1))
+
