7cc39332c4b451894ec767cfe9288c385ed8479d
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2015-11-29 17:28:33 -0500

    Make prepare-email and send private defs

--- src/sendgrid_java_wrapper/core.clj
+++ src/sendgrid_java_wrapper/core.clj
@@ -3,13 +3,13 @@
                          SendGrid$Email
                          SendGrid$Response)))
 
-(defn prepare-email [from subject html]
+(defn- prepare-email [from subject html]
   (-> (SendGrid$Email.)
      (.setFrom from)
      (.setSubject subject)
      (.setHtml html)))
 
-(defn send [{username :api_user password :api_key} email]
+(defn- send [{username :api_user password :api_key} email]
   (.send (SendGrid. username password)
          email))
 
